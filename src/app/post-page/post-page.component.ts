541aa1cf96e4667318ac73122858109216e0590a
adelliceee <themarkbrut@gmail.com>
2021-05-13 13:10:58 +0600

    первая фиксация

---
+++ src/app/post-page/post-page.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
+import {Post} from '../shared/interfaces';
+import {switchMap} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-post-page',
+  templateUrl: './post-page.component.html',
+  styleUrls: ['./post-page.component.scss']
+})
+export class PostPageComponent implements OnInit {
+
+  post$: Observable<Post>
+  ontop: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.post$ = this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      }))
+  }
+
+  goTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDNV8qF1Q2l3AmoPbYVxZCi_H2QR8Xo9iI',
+  fbDbUrl: 'https://angular-blog-b431f-default-rtdb.firebaseio.com/'
+};
